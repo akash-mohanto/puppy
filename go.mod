9552ebf00faac29c415f7c896368f7ecb9ad831e
akash-mohanto <akash.mohanto93@gmail.com>
2023-09-24 00:27:55 +0530

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/akash-mohanto/puppy
 
 go 1.20
+
+require github.com/akash-mohanto/dog v0.0.0-20230923184113-d427da035375 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import "github.com/akash-mohanto/dog"
+
 func Bark() string {
 	return "woof!"
 }
 
 func Barks() string {
 	return "woof! woof! woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Barks())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
